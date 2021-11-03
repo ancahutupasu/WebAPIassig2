6a15e1df5306d8e7eafc504381bb6d359a629c89
Elena <agape.ancaelena@yahoo.com>
2021-11-03 23:07:38 +0100

    a

---
+++ Program.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPIassig2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
